fa4e3a84794764a5374015b4f8f3beac3588d7aa
karottenreibe <karottenreibe@gmail.com>
2009-10-18 21:19:59 +0200

    switched to new layout to please github ;-)

--- tasks/build_task.rb
+++ tasks/build_task.rb
@@ -3,7 +3,8 @@ require 'rake'
 class BuildTask
 
     def initialize( source, target, interpreter = source )
-        source_files = FileList["**/*.#{source}"].exclude('_site')
+        @target = target
+        source_files = FileList["**/.*.#{source}"].exclude('_site')
 
         desc('build all')
         task :build => "build:#{source}"
@@ -11,9 +12,15 @@ class BuildTask
         desc("build all #{source} files")
         task "build:#{source}" do
             source_files.each do |source_file|
-                sh "#{interpreter} #{source_file} #{source_file.ext(target)}"
+                sh "#{interpreter} #{source_file} #{source_to_target(source_file)}"
             end
         end
+    end
+
+    def source_to_target( source_file )
+        basename = File.basename(source_file)[1..-1]
+        dirname  = File.dirname(source_file)
+        File.join(dirname, basename).ext(@target)
     end
 
 end
